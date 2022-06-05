3484e467b33bbe1345c844f242334ecd774c8453
TerraBoii <kostyaborisenko05@gmail.com>
2022-06-05 15:07:48 +0300

    Update app.py: - Pages renamed - Removed some comments - Added some comments - Added quit app binding - Removed unnecessary - Added scrollbar to main page - Added version variable

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from tkinter import Tk, Frame, Button  # Import necessary
+from tkinter import Tk, Frame, Button, Scrollbar, Canvas, Label, Button
+from platform import system
+
+
+_version_ = "0.1"
 
 
 class MainAppBody(Tk):  # App's body
 
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
-        self.title("Title")  # App's title
-        # self.iconbitmap("icon.ico")  # icon file
-        self.geometry(f"{(self.winfo_screenwidth() // 2) + 500}x{(self.winfo_screenheight() // 2) + 200}")  # MIddle of the screen
-        self.minsize(800, 600)  # App's minimal size
-        self.maxsize(self.winfo_screenwidth(), self.winfo_screenheight()) # App's maximum size -> current - screen size
+        self.title(f"What To Do - {_version_}")
+        if system().lower() == "windows": ...
+            # self.iconbitmap("icon.ico")
+        self.geometry(f"{(self.winfo_screenwidth() // 2) + 500}x{(self.winfo_screenheight() // 2) + 200}")
+        self.maxsize(self.winfo_screenwidth(), self.winfo_screenheight() - 31)
+        self.minsize(800, 600)
 
-        # Place page into app
         container = Frame(self)
         container.pack(side='top', fill='both', expand=True)
         container.grid_rowconfigure(0, weight=1)
@@ -19,41 +23,54 @@ class MainAppBody(Tk):  # App's body
 
         self.frames = {}
 
-        frame_collection = (MainPage, DummyPage) # ALL PAGES MUST BR LISTED HERE!!! (Frame classes). All listed pages are going to be constructed
+        frame_collection = (WelcomePage, MainPage)
 
         for frame in frame_collection:
             current_frame = frame(container, self)
             self.frames[frame] = current_frame
             current_frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(MainPage)  # Show main page whenever the app is opened, can add if/else statements to choose which page should be first 
+        self.show_frame(WelcomePage)
 
-    # This is used to show any page (page MUST be listed in frame_collection)
+        self.bind("<Control-q>", lambda _: self.destroy())  # Quit app event
+
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
 
-    # Gets methods of the given page (don't know about variables defined under class, or under methods with "self." in front of them, ex.: self.variable_name)
     def get_page(self, page_class):
         return self.frames[page_class]
 
 
-class MainPage(Frame):
+class WelcomePage(Frame):  # First page that users will see
 
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
         self.controller = controller
 
-        self.switch_pages = Button(self, text="To DummyPage", font=("Arial", 45), command=lambda: controller.show_frame(DummyPage)) # Go to DummyPage page
+        self.switch_pages = Button(self, text="To MainPage", font=("Arial", 45), command=lambda: controller.show_frame(MainPage))
         self.switch_pages.pack(fill='both', pady=2, expand=True)
 
-    def method(self): ...  # Does nothing
 
-
-class DummyPage(Frame):
+class MainPage(Frame):  # Main page + ability to scroll down
 
     def __init__(self, parent, controller):
         Frame.__init__(self, parent, bg="black")
+        self.controller = controller
+
+        canvas = Canvas(self)
+        canvas.pack(side="left", fill="both", expand=True)
+
+        scrollbar = Scrollbar(self, orient="vertical", command=canvas.yview, width=20, bg="#898989", bd=1, troughcolor="#a9a9a9", elementborderwidth=2,
+                              activebackground="#696969", highlightthickness=0)
+        scrollbar.pack(side="right", fill="y")
+
+        canvas.config(yscrollcommand=scrollbar.set)
+        canvas.bind("<Configure>", lambda _: canvas.config(scrollregion=canvas.bbox("all")))
+
+        user_interface = Frame(canvas)
+        ui = user_interface
+        canvas.create_window((0, 0), window=ui, anchor="nw")
 
 
 if __name__ == "__main__":
