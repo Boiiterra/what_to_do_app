417f64c5096103abb1abf1661791b9a8362f22ce
Boiiterra <kostyaborisenko05@gmail.com>
2023-06-04 20:42:22 +0300

    Update +new code for tasks

--- ui.py
+++ ui.py
@@ -1,10 +1,13 @@
 """This file is made for hot reloading in order to test changes in UI/UX"""
 
-from tkinter import Scrollbar, Frame, Label, Toplevel, Button, Canvas
+from tkinter import Scrollbar, Frame, Label, Toplevel, Button, Canvas, Entry
+from tkinter.messagebox import showinfo
 
-FONT = ("Arial", 25)
+FONT = ("Arial", 20)
 ABG = "#d7d7d7"
 BG = "#c7c7c7"
+
+tasks = []  # Will prob use
 
 
 class AutoScrollbar(Scrollbar):
@@ -16,6 +19,70 @@ class AutoScrollbar(Scrollbar):
         else:
             self.pack(side="right", fill="y")
         Scrollbar.set(self, low, high)
+
+
+class NewTask(Toplevel):
+    """Must be reworked for this project"""
+
+    def __init__(self, parent, _location: list):
+        Toplevel.__init__(self, parent, bg="#bbb")
+        view = Frame(self)
+        view.pack(pady=10, padx=10, fill="both", expand=True)
+        self.title("Add task")
+        self.geometry(
+            "320x250"
+            f"+{(self.winfo_screenwidth() - 320) // 2}"
+            f"+{(self.winfo_screenheight() - 250) // 2}"
+        )
+        self.resizable(0, 0)
+        self.transient(parent)
+        self.wait_visibility()
+        self.grab_set()
+
+        def cancel():
+            self.destroy()
+
+        def add():
+            _number = number.get()
+            _letter = letter.get().upper()
+            if _number != "" and _letter != "":
+                number.delete(0, "end")
+                letter.delete(0, "end")
+            else:
+                if _number == "" and _letter == "":
+                    message = "Oh no!"
+                elif _number == "":
+                    message = "Hehe it werks"
+                else:
+                    message = "That is broken"
+                showinfo("Data is missing", message)
+
+        Label(view, text="Add new task", font=15).pack()
+
+        class_cont = Frame(view)
+        class_cont.pack(pady=10)
+
+        Label(class_cont, text="New task:", font=15).grid(row=0, column=0)
+        number = Entry(class_cont, width=2, font=15)
+        number.grid(row=0, column=1)  # Number
+        letter = Entry(class_cont, width=2, font=15)
+        letter.grid(row=0, column=2)  # Letter
+
+        btn_cont = Frame(view)
+        btn_cont.pack(side="bottom", pady=5)
+
+        Button(btn_cont, text="Add task", command=add).grid(
+            row=0, column=0, padx=10, pady=5
+        )
+        Button(btn_cont, text="Cancel", command=cancel).grid(
+            row=0, column=1, padx=10, pady=5
+        )
+
+        self.bind("<q>", lambda _: self.destroy())
+
+
+class TaskRemWarn:
+    pass
 
 
 class Task(Frame):
@@ -34,6 +101,17 @@ class MainPage(Frame):
         bottom = Frame(self, height=100, bg="#02d2d2")
         bottom.pack(fill="both")
 
+        Button(
+            bottom,
+            text="+ New task",
+            font=FONT,
+            bd=0,
+            bg=BG,
+            activebackground=ABG,
+            highlightthickness=0,
+            command=lambda: NewTask(parent, None),
+        ).pack(side="left", pady=(5, 0), padx=(25, 0))
+
         top = Frame(self)
         top.pack(side="bottom", expand=True, fill="both")
         self.top = top
@@ -42,10 +120,14 @@ class MainPage(Frame):
         cnv.pack(side="left", fill="both", expand=True)
         self.canvas = cnv
 
-        scrollbar = AutoScrollbar(top, orient="vertical", command=cnv.yview, width=20)
+        parent.bind("<i>", lambda _: NewTask(parent, None))
+
+        scrollbar = AutoScrollbar(
+            top, orient="vertical", command=cnv.yview, width=20)
 
         cnv.config(yscrollcommand=scrollbar.set)
-        cnv.bind("<Configure>", lambda _: cnv.config(scrollregion=cnv.bbox("all")))
+        cnv.bind("<Configure>", lambda _: cnv.config(
+            scrollregion=cnv.bbox("all")))
 
         parent.canvas = cnv
         parent.top = top
